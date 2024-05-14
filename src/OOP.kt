71863d754e1a02e1252302547166e9c87a9414e4
Moses583 <moisendungu@gmail.com>
2024-05-14 11:19:34 +0300

    Initial commit

---
+++ src/OOP.kt
@@ -0,0 +1,29 @@
+fun main(){
+    println("Hello world")
+    val person1 = Person()
+    person1.name = "Shawn"
+    person1.age = 45
+    person1.isMarried = true
+    person1.laugh()
+    person1.fart("spaghetti")
+    person1.fart("beans")
+
+}
+class Person{
+    var name:String = ""
+    var age:Int = 0
+    var isMarried = false;
+
+    fun laugh(){
+        println("Hahaha")
+    }
+    fun fart(food: String){
+        if (food.startsWith("s")){
+          println("prrrrr")
+        }
+        else{
+            println("bbbrrbbrrbbr")
+        }
+    }
+
+}
