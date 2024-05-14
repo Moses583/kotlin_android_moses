f23cfa88d3d5f4f254c47cd9e636b8717a03377b
Moses583 <moisendungu@gmail.com>
2024-05-14 08:41:36 +0300

    Initial commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    //immutable list
+    val fruits = listOf("mango","pineapple","banana")
+    for (s in fruits){
+        println(s)
     }
+    val filtered = fruits.filter { it.startsWith("m") }
+    println(filtered)
+
+    //immutable set
+    val mySet = setOf(3,4,5,6,"bmw","toyota","mercedes")
+    for (s in mySet)
+        println(s)
+
+    //immutable map
+    val myMap = mapOf(1 to "Kenya", 2 to "Uganda", 3 to "Tanzania", 4 to "Rwanda")
+    for (s in myMap.keys)
+        println(myMap[s])
+
+    val mutableList = mutableListOf(1,2,3,4,5)
+    mutableList.add(6)
+    mutableList[3] = 45
+    for (s in mutableList)
+        println(s)
+
+    val mutableSet = mutableSetOf<Int>(1,2,4,5,67,6)
+    mutableSet.add(34)
+    for (s in mutableSet)
+        println(s)
 }
