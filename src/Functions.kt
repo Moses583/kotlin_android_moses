0a7c91193289c444a5841c564aac258525cef379
Moses583 <moisendungu@gmail.com>
2024-05-14 09:38:21 +0300

    Initial commit

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,4 +1,25 @@
 fun main(){
     println("Hello world")
+    printTrash()
+    printShit("Horseshit")
+    printShit("Bullshit")
+    printShit("Jackshit")
+    val ans1 = doMath(23)
+    println(ans1)
+
 
 }
+//function without arguments
+fun printTrash(){
+    for (i in 1..10){
+        println(i)
+    }
+}
+
+//function with arguments, no return type
+fun printShit(string: String){
+    println("this is the shit to be printed: $string")
+}
+
+//function with arguments and return type
+fun doMath(int: Int): Double = int + Math.random()
