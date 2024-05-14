e24f2624f5a8c0789c53777fab550b0b0e4313e6
Moses583 <moisendungu@gmail.com>
2024-05-14 08:46:49 +0300

    Initial commit

--- src/Main.kt
+++ src/Main.kt
@@ -19,14 +19,25 @@ fun main() {
     for (s in myMap.keys)
         println(myMap[s])
 
+    //mutable list
     val mutableList = mutableListOf(1,2,3,4,5)
     mutableList.add(6)
     mutableList[3] = 45
     for (s in mutableList)
         println(s)
 
+    //mutable set
     val mutableSet = mutableSetOf<Int>(1,2,4,5,67,6)
     mutableSet.add(34)
     for (s in mutableSet)
         println(s)
+
+    //mutable map
+    val myMutableMap = mutableMapOf(1 to "James", 2 to "Kamau", 3 to "Kimemia")
+    myMutableMap[1] = "Wang'ombe"
+    myMutableMap[3] = "Kong'ori"
+    for (s in myMutableMap){
+        println(s)
+    }
+
 }
